7bca0e8ea36f70bee7f5ad49dc5976c3434a85be
Mariano <mfragueiro@externos.itnow.es>
2023-03-18 20:44:22 -0300

    add product view to get all products and add or modify price

--- backend/views/product_of_store_view.py
+++ backend/views/product_of_store_view.py
@@ -14,20 +14,46 @@ class Product_Store_View(APIView):
     store_model = store
 
 
+    def get(self, request, *args, **kwargs):
+        try:
+            # query = request.POST.dict()
+            store = self.store_model.objects.get(name= kwargs['name'])
+            filters = {
+                'store': store,
+            }
 
+            store_product = (self.model.objects
+                        .filter(**filters)
+                        )
+            store_product_serialized = self.serializer(store_product, many=True)
+            return Response(status=status.HTTP_200_OK, data=store_product_serialized.data)
+        except Exception as error:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data= str(error))
 
     def post(self, request, *args, **kwargs):
+        product_price = request.data.pop('price')
+        store_name = request.data.pop('store')
+        product_name = request.data.pop('product')
+        product = (self.product_model.objects.filter(
+           **product_name
+            ))[0]
+        store = self.store_model.objects.filter(**store_name)
+        store_product =  self.model.objects.get(
+            store = store,
+            product = product
+        )
+        if store_product.exists() >0:
+            store_product.price = product_price
+            store_product.save()
+            return Response(status=status.HTTP_201_CREATED, data='price updated')
 
-        product = self.product_model.objects.get(branch=request.data['product']['branch'])
-        store = self.store_model.objects.get(name=request.data['store']['name'])
-        data = {
-            'price' : request.data['price'],
-            'store' : store,
-            'product' : product
-        }
         try:
-            self.model.objects.get_or_create(data)
+            self.model.objects.create(
+                price = product_price,
+                store = store,
+                product = product
+            )
             return Response(status=status.HTTP_201_CREATED, data='price for product added')
         except Exception as error:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data= error)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data= str(error))
 
