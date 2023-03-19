cc3456c037888342ff60d308673ab5fca6767a93
Mariano <mfragueiro@externos.itnow.es>
2023-03-19 19:08:40 -0300

    Add read file function

--- backend/utils/csv_file.py
+++ backend/utils/csv_file.py
@@ -6,19 +6,20 @@ from pathlib import Path
 class CSVFile():
     BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-    def __init__(self, file_name, fields) -> None:
+    def __init__(self, file_name, fields = None) -> None:
         self.file_name = file_name
         self.fields = fields
     
     def read_file(self):
-
-        with open( self.file_name+'.csv', newline='') as csvfile:
+        route_file ='{}/csv_files/{}'.format(self.BASE_DIR, self.file_name)
+        
+        with open( route_file,  encoding="utf8", newline='') as csvfile:
             reader = csv.DictReader(csvfile)
-
+            excel_data = [data for data in reader] 
+        return excel_data
 
     def write_file(self, data):
-        base_dir = self.BASE_DIR
-        route_file ='{}/csv_files/{}.csv'.format(base_dir, self.file_name)
+        route_file ='{}/csv_files/{}.csv'.format(self.BASE_DIR, self.file_name)
         with open(route_file, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(self.fields)
