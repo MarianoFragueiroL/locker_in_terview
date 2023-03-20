b11391cad9419d1c29aafafae757ea4cfa33bb57
Mariano <mfragueiro@externos.itnow.es>
2023-03-20 11:18:53 -0300

    modify open and close time

--- backend/models/store_model.py
+++ backend/models/store_model.py
@@ -5,7 +5,8 @@ from django.db import models
 class store(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=100, unique=True)
-    schedule  = models.TimeField()
+    open_time  = models.TimeField(default='08:00')
+    close_time  = models.TimeField(default='22:00')
 
 
     def __str__(self):
