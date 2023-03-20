75e9313a607f8c82c35e9b397918397912deed9e
Mariano <mfragueiro@externos.itnow.es>
2023-03-20 07:56:11 -0300

    Alter price to float value

--- backend/models/store_product_model.py
+++ backend/models/store_product_model.py
@@ -7,6 +7,6 @@ from .store_model import store
 class store_product(models.Model):
     store = models.ForeignKey(store, on_delete=models.CASCADE)
     product = models.ForeignKey(product, on_delete=models.CASCADE)
-    price  = models.IntegerField(default=0)
+    price  = models.FloatField(default=0)
     amount  = models.IntegerField(default=1)
 
