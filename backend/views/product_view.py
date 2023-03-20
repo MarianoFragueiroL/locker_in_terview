a5b64e623353894754fbcc570f46e5c21b7b4eb2
Mariano <mfragueiro@externos.itnow.es>
2023-03-20 12:25:54 -0300

    modify response of product when is created to get id

--- backend/views/product_view.py
+++ backend/views/product_view.py
@@ -42,7 +42,7 @@ class ProductView(APIView):
             serializer = self.serializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(status=status.HTTP_201_CREATED, data='Product created')
+                return Response(status=status.HTTP_201_CREATED, data=serializer.data)
             return Response(status=status.HTTP_400_BAD_REQUEST, data= serializer.errors)
         except Exception as error:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data= error)
